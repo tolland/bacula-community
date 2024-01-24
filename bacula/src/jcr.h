0624c8ab68c273900a57446e40a6b5735d05b1b9
Eric Bollengier <eric@baculasystems.com>
2024-01-24 12:04:40 +0100

    Fix org#2704 about old FD compatibility
    Old client (prior to 11.0.0) job messages are silently dropped from
    the director logs. The Job status is correct, but the messages are
    not displayed in the catalog or the bacula-dir.log.
    
    The workaround is to enable the local log on each impacted FD,
    or upgrade them to 13.0.x.
    
    This fix will detect the client version and apply a fix dynamically
    when getting a message.

--- bacula/src/jcr.h
+++ bacula/src/jcr.h
@@ -433,6 +433,7 @@ public:
    int32_t FDVersion;                 /* File daemon version number */
    int32_t SDVersion;                 /* Storage daemon version number */
    int64_t spool_size;                /* Spool size for this job */
+   uint64_t client_version;           /* Client version as a number */
    utime_t snapshot_retention;        /* Snapshot retention (from Client/Job resource) */
    volatile bool sd_msg_thread_done;  /* Set when Storage message thread done */
    bool wasVirtualFull;               /* set if job was VirtualFull */
