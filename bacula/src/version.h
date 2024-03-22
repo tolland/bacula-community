3a5a159c787c6f7074ac6842b22fc965b00cb584
Eric Bollengier <eric@baculasystems.com>
2024-03-22 10:25:30 +0100

    update version

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -24,14 +24,14 @@
 #define COMMUNITY 1      /* Define to create a Windows community binary */
 
 /* Note: there can be only *one* VERSION in this file */
-#define VERSION "15.0.1"
-#define BDATE   "13 February 2024"
-#define LSMDATE "13Feb24"
+#define VERSION "15.0.2"
+#define BDATE   "21 March 2024"
+#define LSMDATE "21Mar24"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
-#define PROG_COPYRIGHT "Copyright (C) %d-2023 Kern Sibbald.\n"
-#define BYEAR "2023"       /* year for copyright messages in progs */
+#define PROG_COPYRIGHT "Copyright (C) %d-2024 Kern Sibbald.\n"
+#define BYEAR "2024"       /* year for copyright messages in progs */
 
 /*
  * Versions of packages needed to build Bacula components
