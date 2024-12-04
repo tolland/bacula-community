453dc8ef9e41ba21b7fc4b69ab49e5f31f40b067
Eric Bollengier <eric@baculasystems.com>
2024-12-04 09:06:22 +0100

    bpipe: Fix org#2737 About segfault with bpipe
    p_ctx->cmd is allocated with strdup() and released with
    smartalloc and it causes the segfault.
    
    Thanks to Marcin for this report.

--- bacula/src/plugins/fd/bpipe-fd.c
+++ bacula/src/plugins/fd/bpipe-fd.c
@@ -202,7 +202,9 @@ static bRC freePlugin(bpContext *ctx)
    if (!p_ctx) {
       return bRC_Error;
    }
-   bfree_and_null(p_ctx->cmd);
+   if (p_ctx->cmd) {
+      free(p_ctx->cmd);                  /* free any allocated command string */
+   }
    free_and_null_pool_memory(p_ctx->restore_obj_buf);
    if (p_ctx->rop_writer) {
       restoreobj *rop;
