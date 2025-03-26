3c467f984cdb85a62d3ce12c2572b3d0d5db932b
Marcin Haba <marcin.haba@bacula.pl>
2025-03-26 05:35:29 +0100

    baculum: Update version

--- gui/baculum/protected/Common/Modules/Params.php
+++ gui/baculum/protected/Common/Modules/Params.php
@@ -31,7 +31,7 @@ namespace Baculum\Common\Modules;
  */
 class Params extends CommonModule {
 
-	const BACULUM_VERSION = '15.0.2';
+	const BACULUM_VERSION = '15.0.3';
 
 	public static $months = array(
 		'jan' => 'January',
